4f78556ee7a0580cb0f02ecb6b4df8a1c38b19ea
Breno Borges <breno.borges.15@outlook.com>
2024-08-03 11:42:12 -0300

    Start Project

---
+++ src/main/java/br/com/tripleahackathon/help_state/HelpStateApplication.java
@@ -0,0 +1,13 @@
+package br.com.tripleahackathon.help_state;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpStateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpStateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tripleahackathon/help_state/HelpStateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tripleahackathon.help_state;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpStateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
