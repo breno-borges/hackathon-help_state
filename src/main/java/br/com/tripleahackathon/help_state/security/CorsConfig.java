ccdab51358c854e58dd7f62d0d4f2a39e502bd68
Breno Borges <breno.borges.15@outlook.com>
2024-08-04 17:48:14 -0300

    Implementada configuração de CORS

--- src/main/java/br/com/tripleahackathon/help_state/security/CorsConfig.java
+++ src/main/java/br/com/tripleahackathon/help_state/security/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8080")
+                        .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS, PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true);
